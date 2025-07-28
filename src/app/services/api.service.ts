c9c69f36697773cbc3c7b611ba50d9ee3ec3b7e0
cleddrencedalupan <cleddrencedalupan@gmail.com>
2025-07-28 11:53:02 +0000

    deploy v0

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = '/api/'; // Proxy to backend
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://isuper.reviewcenterphil.com/api/'
 };
